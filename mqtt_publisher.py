f77e6df536767631d677a48402d4e7d7a63031dd
Jaime <jaime.escobar.mtz@gmail.com>
2019-11-24 16:36:59 -0700

    Update to use in maqiatto.com

--- mqtt_publisher.py
+++ mqtt_publisher.py
@@ -20,26 +20,26 @@ def on_message(client, userdata, message):
     print('Message received:', m)
     if m == '1':
         print("Sending humidity...")
-        client.publish("teempo/pub/hum", dht11.get_humidity())
+        client.publish("jaime.escobar.mtz@gmail.com/IoT/Teempo/hum", dht11.get_humidity())
         print("Humidity sent")
     elif m == '0':
         print("Sending temperature...")
-        client.publish("teempo/pub/temp", dht11.get_temperature())
+        client.publish("jaime.escobar.mtz@gmail.com/IoT/Teempo/temp", dht11.get_temperature())
         print("Temperature sent")
 
 
 # Connected = False  # global variable for the state of the connection
 
-broker_address = "localhost"
-user = "user1"
-password = "1234"
+broker_address = "maqiatto.com"
+user = "jaime.escobar.mtz@gmail.com"
+password = "IoTGuaymas"
 
 client = Client("Publicador")  # create new instance
 client.username_pw_set(user, password=password)  # set username and password
 client.on_connect = on_connect  # attach function to callback
-client.connect(broker_address)  # connect to broker
+client.connect(broker_address, port=1883)  # connect to broker
 client.on_message = on_message  # attach function to callback
-client.subscribe("teempo/client")
+client.subscribe("jaime.escobar.mtz@gmail.com/IoT/Teempo/client")
 client.loop_forever()
 # client.loop_start()  # start the loop
 
